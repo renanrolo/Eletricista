7669d5495717b8d195898f5a055cfc2dd4f5c650
Renan Rolo <renanrolo@hotmail.com>
2022-06-23 08:24:20 -0400

    home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FiosComponent } from './pages/fios/fios.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: 'fios', component: FiosComponent },
-  { path: 'home', component: FiosComponent },
+  { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from 'src/shared/shared.module';
 import { FiosComponent } from './pages/fios/fios.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FiosComponent,
+    HomeComponent,
     HeaderComponent
   ],
   imports: [
